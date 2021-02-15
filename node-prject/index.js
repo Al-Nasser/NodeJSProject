5b3842d25d0bcd281495b1ca1e7f36934ed02ea2
anaamane <ahmed.naamane@example.com>
2021-02-15 23:27:49 +0100

    ---completed---

--- node-prject/index.js
+++ node-prject/index.js
@@ -1,51 +1,65 @@
-const path = require('path');
-const expressEdge = require('express-edge');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Post = require('./database/models/Post');
+const expressEdge = require("express-edge");
+const express = require("express");
+const edge = require("edge.js");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const connectFlash = require("connect-flash");
 
+ 
+const createPostController = require('./controllers/createPost');
+const homePageController = require('./controllers/homePage');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const createUserController = require("./controllers/createUser");
+const storeUserController = require('./controllers/storeUser');
+const loginController = require("./controllers/login");
+const loginUserController = require('./controllers/loginUser');
+const logoutController = require('./controllers/logout');
+
+ 
 const app = new express();
-
-app.use(express.static('public'));
-app.use(expressEdge.engine);
-
+ 
 mongoose.connect('mongodb://blog:blog@localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true  })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
  
-    app.set('views', __dirname + '/views');
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-
-
-
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
-});
-
-app.get('/about.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
-});
+app.use(connectFlash());
  
-app.get('/contact.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-});
+const mongoStore = connectMongo(expressSession);
  
-app.get('/post.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-});
-
-app.get('/posts/new', (req, res) => {
-    res.render('create')
-});
- 
-app.post('/posts/store', (req, res) => {
-    Post.create(req.body, (error, post) => {
-        res.redirect('/index.html')
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
     })
+}));
+ 
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/views');
+
+const storePost = require('./middleware/storePost');
+const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
+ 
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", auth, createPostController);
+app.post("/posts/store", auth, storePost, storePostController);
+app.get("/auth/login", redirectIfAuthenticated, loginController);
+app.post("/users/login", redirectIfAuthenticated, loginUserController);
+app.get("/auth/register", redirectIfAuthenticated, createUserController);
+app.post("/users/register", redirectIfAuthenticated, storeUserController);
+app.get("/auth/logout", logoutController);
+
+ 
+app.use('*', (req, res, next) => {
+    edge.global('auth', req.session.userId)
+    next()
 });
 
 app.listen(4000, () => {
